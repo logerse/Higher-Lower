4e3769d90ee27d57d56a130e14670715a3a9f615
logerse <kotovcod@gmail.com>
2018-04-24 22:42:25 +0300

    +lose

--- GUI.py
+++ GUI.py
@@ -4,8 +4,16 @@ class Lables:
 	def __init__(self,x=2,y=1,text_="HIGHER\nLOWER"):
 		self.obj=Label(mainframe,text=text_,fg="#485054",bg='#e0dbd7',font="Sans-serif 70",bd=80)
 		self.obj.grid(column=x,row=y,sticky=(N,S))
-	def check(self,points):
-		self.obj.config(text="Points:"+points)
+	def check(self,points,text_='',color=""):
+		if text_=='':
+			self.obj.config(text="Points:"+points)
+		else:
+			self.obj.config(text=text_,fg=color)
+			if text_[0]=='P':
+				self.obj.config(font="Sans-serif 30")
+			root.geometry('580x500')
+	def dlt(self):
+		self.obj.destroy()
 class buttons:
 	def __init__(self,x,y,cmmnd,s_,text_="-"):
 		self.but=Button(mainframe,text=text_,fg="#503457",bg="#e0dbd7",font="Sans-serif 30",width=9,height=3)
@@ -13,10 +21,23 @@ class buttons:
 		self.but.bind("<Button-1>",cmmnd)
 	def check(self,text_=''):
 		self.but.config(text=text_)
+	def dlt(self):
+		self.but.destroy()
+
+def miss():
+	global points
+	obj1.check(0,"You lose",'#ff335c')
+	obj2.dlt()
+	obj3.dlt()
+	Points.check(0,"Points:"+str(points),'#ff335c') 
+
 def chs_1(event):
 	global points
 	if maincode.checkans(1):
-		points+=1	
+		points+=1
+	else:
+		miss()
+		return 0	
 	maincode.refresh()
 	r3fr3sH()
 	
@@ -24,12 +45,17 @@ def chs_2(event):
 	global points
 	if maincode.checkans(2):
 		points+=1
+	else:
+		miss()
+		return 0
 	maincode.refresh()
 	r3fr3sH()
+
 def r3fr3sH():
 	Points.check(str(points))
 	obj2.check(maincode.lastname)
 	obj3.check(maincode.name)
+
 from tkinter import *
 import time
 import maincode
